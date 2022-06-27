e32685247214d83dd28864a8dd4c590e6e7b348e
JK99837 <104741801+JK99837@users.noreply.github.com>
2022-06-27 11:31:17 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+// function fruit() {
+//    if(true) {
+//        var typeOfFruit = 'apples';
+//    }
+//    console.log(typeOfFruit);
+// }
+
+
+// if (true) {
+// let color = 'teal';
+// console.log(color);
+// if(true) {
+// console.log(color);    
+// }
+//  }
+
+// function football() {
+//    let footballPlay = "Pass";
+//    console.log(footballPlay);
+// }
+
+// var i = 'hello';
+// for (var i = 0; i <= 3; i++) {
+//    console.log(i);
+// }
+// console.log(i);
+
+// var i = 'hello';
+// for (let i = 0; i <= 3; i++) {
+//    console.log(i);
+// }
+// console.log(i);
+
+// let color = 'red'; 
+// function getColor(num) {
+//    let color = 'blue';
+//    if (num < 3) {
+//    let color = 'purple';
+//    console.log(color);
+//    }
+//    return color;
+// }
+// getColor(2);
+
+// const PI = 3.14159;
+// if (true) {
+//    const PI = 4.561234;
+//    console.log(PI);
+// }
+// console.log(PI);
+
+// ------------------------variable hoisting--------------------------------
+
+// var
+// console.log(dogs);
+// var dogs = [ 'Ollie', 'Pepper', 'Sally', 'Randy', 'Nancy',
+// 'Emily' ];
+// console.log(dogs); 
+
+// let
+// console.log(dogs);
+// let dogs = [ 'Ollie', 'Pepper', 'Sally', 'Randy', 'Nancy',
+// 'Emily' ];
+// console.log(dogs); 
+
+
+// ------------------------------Notes---------------------------------------
+// Always use let & const
+// const cannot reasign values
+// use let if you need to reasign a value
+
